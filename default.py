13ed0289203ba317787ede5facbcbcf96f80c691
clement3872 <clement.noisy78@gmail.com>
2024-04-24 05:35:09 +0200

    changing how the code works

--- default.py
+++ default.py
@@ -1,8 +1,6 @@
-import random as rd
-
 
 class LFSR:
-    def __init__(self, status, xor_list, last_output=None):
+    def __init__(self,size, status, xor_list, last_output=None):
         """
         args:
             - status: a string of bits (int of 0 and 1)
@@ -11,36 +9,37 @@ class LFSR:
                           else None
         """
         # some asserts for "security"
-        assert "1" in status, "There should be a 1 in the list `status`"
+        assert type(status) is int, "`status` should be of type int (bin)"
+        assert status > 0, "There should be a 1 in the list `status`"
+        assert type(size) is int and size>=status.bit_count(), "The size is wrong"
 
-        self.initial_status = [0 if el=="0" else 1 for el in status]
+        self.size = size-1
+        self.initial_status = status
         self.xor_list = []
         for el in xor_list:
             if el != 0:
-                self.xor_list.append(len(self.initial_status)-1-el)
+                self.xor_list.append(el)
         self.xor_list.sort()
 
-        self.current_status = self.initial_status.copy()
+        self.current_status = self.initial_status
         self.last_output = last_output
         self.nb_iteration = 0
     
-    def next_status(self):
+    def next(self):
         """Returns the next bit of the LFSR"""
-        self.last_output = self.current_status[-1]
+        self.last_output = 0b1 & self.current_status
         tmp = self.last_output
         for i in self.xor_list:
-            if i != 0:
-                tmp = self.current_status[i]^tmp # `^` is a XOR
+            tmp = (self.current_status>>i & 0b1)^tmp # `^` is a XOR
         
         # shifting the list
-        self.last_output = self.current_status.pop()
-        self.current_status = [tmp] + self.current_status
+        self.current_status = (self.current_status>>1) + tmp*(1 << self.size)
         self.nb_iteration += 1
 
         return self.last_output
     
     def reset(self):
-        self.current_status = self.initial_status.copy()
+        self.current_status = self.initial_status
         self.last_output = None
         self.nb_iteration = 0
     
@@ -50,9 +49,9 @@ class LFSR:
         """
         # check duplicates
         old_last_output = self.last_output # we have to back up this
-        start = self.current_status.copy()
-        for i in range(2**len(self.initial_status) - 2): # First one is included
-            self.next_status()
+        start = self.current_status
+        for i in range(2**(self.size+1) - 2): # First one is included
+            self.next()
             if self.current_status == start:
                 print("There is a duplicate at iteration number",i+1)
                 print("start:", start)
@@ -60,12 +59,12 @@ class LFSR:
                 print("current_status", self.current_status)
                 # reset status
                 self.last_output = old_last_output
-                self.current_status = start.copy()
+                self.current_status = start
                 self.nb_iteration -= i
                 return False
         # reset status
         self.last_output = old_last_output
-        self.current_status = start.copy()
+        self.current_status = start
         self.nb_iteration -= i
         return True
 
@@ -83,26 +82,16 @@ class CSS:
         self.c = 0
     
     def next(self):
-        """
-        Returns the next 8 bits of the CSS in a string
-        """
-        s = ""
-        x = ""
-        for el in reversed([self.lfsr1.next_status() for _ in range(8)]):
-            x += str(el)
-        x = int(x, 2) 
-        y = ""
-        for el in reversed([self.lfsr2.next_status() for _ in range(8)]):
-            y += str(el)
-        y = int(y, 2)
+        """Returns the 8 next bits of the CSS as an int"""
+        x = 0
+        for i in range(8): x += self.lfsr1.next() << i
+        y = 0
+        for i in range(8): y += self.lfsr2.next() << i
 
-        res = (x+y+self.c)%256
-        res = bin(res)[2:]
-        while len(res) < 8:
-            res = "0"+res
+        z = (x+y+self.c)%256
 
         self.c = 1 if x+y>255 else 0
-        return res
+        return z
 
     def encode(self, m):
         """
@@ -111,24 +100,13 @@ class CSS:
         -
         It does not use `next` function, it's faster
         """
-        s = ""
+        res = 0 
         for i in range(len(m)//2):
-            x = ""
-            for el in reversed([self.lfsr1.next_status() for _ in range(8)]):
-                x += str(el)
-            x = int(x, 2) 
-            y = ""
-            for el in reversed([self.lfsr2.next_status() for _ in range(8)]):
-                y += str(el)
-            y = int(y, 2)
+            x = self.next()
+            res = (res<<8) +x# self.next()
 
-            res = (x+y+self.c)%256
-            tmp = int(m[2*i]+m[2*i+1], 16)
-            s += hex(tmp^res)[2:]
-
-            self.c = 1 if x+y>255 else 0
         self.reset()
-        return s
+        return hex(res^int(m,16))[2:]
     
     def decode(self, c):
         # It's symmetrical
@@ -140,9 +118,9 @@ class CSS:
 
 def test0():
     # First example from the PDF (on 8 bits)
-    start = "10010110"
+    start = 0b10010110
     xor_list = (2,3,4)
-    l = LFSR(start, xor_list)
+    l = LFSR(8,start, xor_list)
     l.check_duplicates()
 
 def test1():
@@ -152,10 +130,10 @@ def test1():
     (question 1)
     """
     n = 17
-    start = "1" + (n-1)*"0"
+    start = 1 << (n-1) #"1" + (n-1)*"0"
     xor_list = (14,0)
 
-    l = LFSR(start, xor_list) 
+    l = LFSR(n, start, xor_list) 
     l.check_duplicates()
 
 def test2():
@@ -165,10 +143,10 @@ def test2():
     (question 2)
     """
     n = 25
-    start = "1" + (n-1)*"0"
+    start = 1 << (n-1) 
     xor_list = (12,4,3,0)
 
-    l = LFSR(start, xor_list) 
+    l = LFSR(n, start, xor_list) 
     l.check_duplicates()
 
 
@@ -177,14 +155,14 @@ def test3():
     (question 3)
     """
     n = 17
-    start = "1" + (n-1)*"0"
+    start = 1 << (n-1) 
     xor_list = (14,0)
-    l1 = LFSR(start, xor_list) 
+    l1 = LFSR(n, start, xor_list) 
 
     n = 25
-    start = "1" + (n-1)*"0"
+    start = 1 << (n-1) 
     xor_list = (12,4,3,0)
-    l2 = LFSR(start, xor_list) 
+    l2 = LFSR(n, start, xor_list) 
 
     css = CSS(l1,l2)
     m = "ffffffffff"
@@ -195,29 +173,13 @@ def test3():
     print(f"Encoded message: {c}")
     print(f"Decoding the encoded message: {css.decode(c)}") # verification
 
-
-def pates():
-    n = 17
-    start = "1" + (n-1)*"0"
-    xor_list = (14,0)
-    l1 = LFSR(start, xor_list) 
-
-    n = 25
-    start = "1" + (n-1)*"0"
-    xor_list = (12,4,3,0)
-    l2 = LFSR(start, xor_list) 
-
-    css = CSS(l1,l2)
-    for _ in range(10):
-        print(css.next())
-    
     
 if __name__ == "__main__":
     # NOTE : not every LFSR have a "cycle" of 2^n - 1 iterations 
 
-    #test0()
-    #test1()
-    #test2() # This test may take a bit of time
-    #test3() # CSS 
+    # test0()
+    # test1()
+    # test2() # This test may take a bit of time
+    # test3()
 
     pass
